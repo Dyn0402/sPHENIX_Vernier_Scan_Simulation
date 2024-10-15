711fddd9630a91ef11963053ac5ccf79fc9f3aba
Dylan Neff <Dyn0402@hotmail.com>
2024-10-15 19:42:19 +0200

    luminosity_calculation.py added angle_lumi_plot for analysis note

--- luminosity_calculation.py
+++ luminosity_calculation.py
@@ -10,10 +10,10 @@ Created as sPHENIX_Vernier_Scan_Simulation/luminosity_calculation.py
 
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.core.pylabtools import figsize
 
 from BunchCollider import BunchCollider
 from Measure import Measure
+from vernier_z_vertex_fitting import read_cad_measurement_file
 
 
 def main():
@@ -25,7 +25,10 @@ def main():
     longitudinal_fit_path = f'{base_path}CAD_Measurements/VernierScan_{vernier_scan_date}_COLOR_longitudinal_fit.dat'
     # head_on_luminosity(longitudinal_fit_path)
     # head_on_luminosity_simple_gaus()
-    head_on_luminosity_plot()
+    # head_on_luminosity_plot()
+    # angle_luminosity_plot()
+    # moller_factor_test()
+    cad_parameters_luminosity(cad_measurement_path, longitudinal_fit_path)
     print('donzo')
 
 
@@ -140,7 +143,32 @@ def head_on_luminosity_simple_gaus():
     plt.show()
 
 
+def moller_factor_test():
+    # Important parameters
+    y_offset_nom = -0.
+    x_offset_nom = 0.
+    angle_y_blue, angle_y_yellow = 0., 2e-3
+    angle_x_blue, angle_x_yellow = 0., 0.
+
+    collider_sim = BunchCollider()
+    collider_sim.set_bunch_rs(np.array([x_offset_nom, y_offset_nom, -6.e6]), np.array([0., 0., +6.e6]))
+    collider_sim.set_bunch_crossing(angle_x_blue, angle_y_blue, angle_x_yellow, angle_y_yellow)
+    collider_sim.bunch1.calculate_r_and_beta()
+    collider_sim.bunch2.calculate_r_and_beta()
+
+    print(f'v1: {collider_sim.bunch1.beta * collider_sim.bunch1.c}')
+    print(f'v2: {collider_sim.bunch2.beta * collider_sim.bunch2.c}')
+
+    print(f'Moller factor: {collider_sim.get_relativistic_moller_factor() / collider_sim.bunch1.c}')
+    print(f'Moller factor difference from head on: '
+          f'{((collider_sim.get_relativistic_moller_factor() / collider_sim.bunch1.c) - 2) * 100}%')
+
+
 def head_on_luminosity_plot():
+    """
+    Plot luminosity z-densities for combinations of beam widths and bunch lengths. Compare to analytical luminosity.
+    :return:
+    """
     # Beam widths and bunch lengths
     beam_widths = [160., 180.]  # um
     bunch_lengths = [1.1e6, 1.3e6]  # um
@@ -150,13 +178,11 @@ def head_on_luminosity_plot():
 
     mu = u'\u03BC'
 
-    fig, ax = plt.subplots(figsize=(8, 4.5))
+    fig, ax = plt.subplots(figsize=(8, 4))
 
     # Stacking position for annotations
-    annotation_offset = 0.25
-    y_offset = 0.98  # Start at 100% of the plot height
-
-    sim_lumis = {}
+    annotation_offset = 0.2
+    y_offset = 0.88  # Start at 100% of the plot height
 
     for bw in beam_widths:
         # Calculate analytical luminosity
@@ -185,20 +211,158 @@ def head_on_luminosity_plot():
     ax.set_ylim(bottom=0)
     ax.set_xlim(left=-349, right=349)
     ax.set_xlabel('z (cm)')
-    ax.set_ylabel('Single Particle Luminosity')
+    ax.set_ylabel('Naked Luminosity z Density')
+
+    # Add an annotation with the analytical luminosity equation
+    ax.annotate('Analytical Luminosity = 1 / 4πσ²', xy=(0.99, 0.97), xycoords='axes fraction', ha='right', va='top',
+                bbox=dict(boxstyle='round', facecolor='salmon', alpha=0.3))
+
     ax.legend(loc='upper left')
     fig.tight_layout()
 
     plt.show()
 
 
-def simulate_luminosity(bw_nom, bunch_length):
+def angle_luminosity_plot():
+    """
+    Plot luminosity z-densities for different crossing angles. Compare to analytical luminosity.
+    :return:
+    """
+    # bunch_2_y_angles = [0., 1e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3, 7e-3, 8e-3, 9e-3, 10e-3]  # rad
+    bunch_1_y_angles = [0., 1e-3, 10e-3]  # rad
+    bunch_2_y_angle = 0.  # rad
+    bw = 160.  # um
+    bl = 1.1e6  # um
+
+    mu = u'\u03BC'
+
+    fig, ax = plt.subplots(figsize=(8, 4))
+
+    # Stacking position for annotations
+    annotation_offset = 0.2
+    y_offset = 0.88  # Start at 100% of the plot height
+
+    for angle in bunch_1_y_angles:
+        # Calculate analytical luminosity
+        analytical_lum = analytical_luminosity(bw, bl, angle)
+        annotate_str = f'Luminosity for {angle * 1e3:.0f} mrad crossing angle\nAnalytical = {analytical_lum:.2e}'
+
+        # Simulate luminosity
+        print(f'Simulating crossing angle: {angle * 1e3:.0f} mrad')
+        print(f'Analytical luminosity: {analytical_lum:.2e}')
+        zs, z_dist, simulated_luminosity = simulate_luminosity(bw, bl, angle, bunch_2_y_angle)
+
+        # Plot z distribution
+        label = f'Crossing angle {angle * 1e3:.0f} mrad'
+        ax.plot(zs, z_dist, label=label)
+
+        # Calculate percent difference
+        percent_diff = 100 * (simulated_luminosity - analytical_lum) / analytical_lum
+        annotate_str += f'\nSimulated = {simulated_luminosity:.2e} ({percent_diff:.2f}%)'
+
+        # Annotate percent difference for both bunch lengths
+        ax.annotate(annotate_str, xy=(0.99, y_offset), xycoords='axes fraction', ha='right', va='top',
+                    bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.7))
+        y_offset -= annotation_offset
+
+    ax.axhline(0, color='black', linewidth=0.5)
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(left=-349, right=349)
+    ax.set_xlabel('z (cm)')
+    ax.set_ylabel('Naked Luminosity z Density')
+
+    # Add an annotation with the analytical luminosity equation
+    ax.annotate('Analytical Luminosity = S / 4πσ²', xy=(0.99, 0.97), xycoords='axes fraction', ha='right', va='top',
+                bbox=dict(boxstyle='round', facecolor='salmon', alpha=0.3))
+
+    ax.legend(loc='upper left')
+    fig.tight_layout()
+
+    plt.show()
+
+
+def cad_parameters_luminosity(cad_measurement_path, longitudinal_fit_path):
+    """
+    Simulate the bunch collision given CAD parameters and calculate the luminosity.
+    :param cad_measurement_path:
+    :param longitudinal_fit_path:
+    :return:
+    """
+    cad_data = read_cad_measurement_file(cad_measurement_path)
+    print(cad_data)
+    scan_orientation = 'Horizontal'
+    head_on_cad_data = cad_data[cad_data['orientation'] == scan_orientation].iloc[0]
+
+    # Important parameters
+    bw_nom = 180.  # um Width of bunch
+    beta_star_nom = 85.
+    mbd_online_resolution = None  # cm MBD resolution on trigger level
+    yellow_length_scaling, blue_length_scaling = head_on_cad_data['yellow_bunch_length_scaling'], head_on_cad_data['blue_bunch_length_scaling']
+
+    # Will be overwritten by CAD values
+    y_offset_nom = 0.
+    angle_nom = +0.14e-3
+
+    collider_sim = BunchCollider()
+    collider_sim.set_bunch_rs(np.array([0., y_offset_nom, -6.e6]), np.array([0., 0., +6.e6]))
+    collider_sim.set_bunch_beta_stars(beta_star_nom, beta_star_nom)
+    collider_sim.set_bunch_sigmas(np.array([bw_nom, bw_nom]), np.array([bw_nom, bw_nom]))
+    collider_sim.set_bunch_crossing(0, angle_nom, 0, 0)
+    collider_sim.set_gaus_smearing_sigma(mbd_online_resolution)
+    blue_fit_path = longitudinal_fit_path.replace('_COLOR_', '_blue_')
+    yellow_fit_path = longitudinal_fit_path.replace('_COLOR_', '_yellow_')
+    collider_sim.set_longitudinal_fit_parameters_from_file(blue_fit_path, yellow_fit_path)
+    collider_sim.set_longitudinal_fit_scaling(blue_length_scaling, yellow_length_scaling)
+
+    offset = head_on_cad_data['offset_set_val'] * 1e3  # mm to um
+    blue_angle, yellow_angle = -head_on_cad_data['bh8_avg'] / 1e3, -head_on_cad_data['yh8_avg'] / 1e3  # mrad to rad
+
+    if scan_orientation == 'Horizontal':
+        collider_sim.set_bunch_offsets(np.array([offset, 0.]), np.array([0., 0.]))
+    elif scan_orientation == 'Vertical':
+        collider_sim.set_bunch_offsets(np.array([0., offset]), np.array([0., 0.]))
+
+    collider_sim.set_bunch_crossing(blue_angle, 0, yellow_angle, 0)
+
+    collider_sim.run_sim_parallel()
+    zs, z_dist = collider_sim.get_z_density_dist()
+
+    luminosity = collider_sim.get_naked_luminosity()
+    print(f'Luminosity: {luminosity:.2e}')
+
+    analytic_lumi = analytical_luminosity(bw_nom)
+    print(f'Analytical luminosity: {analytic_lumi:.2e}')
+
+    percent_differs = 100 * (luminosity - analytic_lumi) / analytic_lumi
+    print(f'Percent difference: {percent_differs:.2f}%')
+
+    fig, ax = plt.subplots()
+    ax.plot(zs, z_dist)
+    ax.set_xlabel('z (cm)')
+    ax.set_ylabel('Naked Luminosity z Density')
+    fig.tight_layout()
+
+    plt.show()
+
+
+def simulate_luminosity(bw_nom=None, bunch_length=None, angle_y_blue=None, angle_y_yellow=None):
+    if bw_nom is None:
+        bw_nom = 160.
+    if bunch_length is None:
+        bunch_length = 1.1e6
+    if angle_y_blue is None:
+        angle_y_blue = 0.
+    if angle_y_yellow is None:
+        angle_y_yellow = 0.
+
     collider_sim = BunchCollider()
     collider_sim.set_bunch_rs(np.array([0., -0., -6.e6]), np.array([0., 0., +6.e6]))
     collider_sim.set_bunch_beta_stars(None, None)
     collider_sim.set_bunch_sigmas(np.array([bw_nom, bw_nom, bunch_length]), np.array([bw_nom, bw_nom, bunch_length]))
-    collider_sim.set_bunch_crossing(0., 0., 0., 0.)
-    collider_sim.set_z_bounds((-345 * 1e4, 345 * 1e4))
+    collider_sim.set_bunch_crossing(0., angle_y_blue, 0., angle_y_yellow)
+    z_lim = 345 / (angle_y_blue * 1e3 + 1)
+    print(f'Z limit: {z_lim}')
+    collider_sim.set_z_bounds((-z_lim * 1e4, z_lim * 1e4))
 
     collider_sim.run_sim_parallel()
 
@@ -209,8 +373,11 @@ def simulate_luminosity(bw_nom, bunch_length):
     return zs, z_dist, luminosity
 
 
-def analytical_luminosity(width):
-    return 1 / (4 * np.pi * width**2)
+def analytical_luminosity(width, length=None, angle=None):
+    lumi = 1 / (4 * np.pi * width**2)  # Head on luminosity
+    if length is not None and angle is not None:
+        lumi *= 1. / np.sqrt(1 + (width / length * np.tan(angle / 2))**2) / np.sqrt(1 + (length / width * np.tan(angle / 2))**2)
+    return lumi
 
 
 if __name__ == '__main__':

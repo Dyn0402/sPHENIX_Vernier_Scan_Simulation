96d6a7362bc3421c715da27ff02cef1dfe54a4f9
Dylan Neff <Dyn0402@hotmail.com>
2025-03-24 23:06:08 +0100

    trigger_rate_vs_offset_comparison.py work on plots for AN

--- plotting/trigger_rate_vs_offset_comparison.py
+++ plotting/trigger_rate_vs_offset_comparison.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit as cf
 import pandas as pd
 
-from vernier_z_vertex_fitting import read_cad_measurement_file, get_cw_rates
+from vernier_z_vertex_fitting import read_cad_measurement_file
 from analyses.phenix_style_hourglass_analysis import vernier_scan_fit_bkg
 from Measure import Measure
 
@@ -22,8 +22,11 @@ from Measure import Measure
 def main():
     if platform.system() == 'Linux':
         base_path = '/local/home/dn277127/Bureau/vernier_scan/'
+        out_path = None
     else:  # Windows
         base_path = 'C:/Users/Dylan/Desktop/vernier_scan/'
+        out_path = f'C:/Users/Dylan/OneDrive - UCLA IT Services/Research/Saclay/sPHENIX/Vernier_Scan/Analysis_Note/Cross_Checks/'
+
 
     # Gaussian approximation with bad beam width luminosity
     f_beam = 78.4  # kHz
@@ -42,7 +45,6 @@ def main():
         cad_measurement_path = f'{base_path}CAD_Measurements/VernierScan_{scan_date}_combined.dat'
 
         cad_data = read_cad_measurement_file(cad_measurement_path)
-        cw_rates = get_cw_rates(cad_data)
 
         for orientation in orientations:
             print(f'\n{orientation}')
@@ -59,7 +61,7 @@ def main():
             bw_df_offsets = bw_fit_df[f'bunch1_offset_{orientation_xy}']
             bw_df_naked_lumis = bw_fit_df[f'naked_luminosity'].to_numpy()
             # bw_df_lumis = bw_df_naked_lumis * mb_to_um2 * (f_beam * 1e3) * n_blue * n_yellow
-            bw_df_amps = bw_fit_df[f'amplitude'].to_numpy()
+            # bw_df_amps = bw_fit_df['amplitude'].to_numpy()
             bw_df_lumis = bw_df_naked_lumis * np.max(rates) / np.max(bw_df_naked_lumis)
 
             # Sort bw_df_offsets and bw_df_lumis together by bw_df_offsets
@@ -84,22 +86,28 @@ def main():
 
             xs_plot = np.linspace(np.min(offsets), np.max(offsets), 1000)
 
-            width_str = f'CW Width: {pmeas_cw[1]} μm\nSim Width: {pmeas_sim[1]} μm'
+            width_str = f'MBD Measured Fit Width: {pmeas_cw[1]} μm\nNumerical Fit Width: {pmeas_sim[1]} μm'
 
             # Plot
             fig, ax = plt.subplots()
-            ax.plot(offsets, rates, marker='o', c='k', ls='none', label='CW Rates')
-            ax.plot(xs_plot, vernier_scan_fit_bkg(xs_plot, *popt_cw), '--', c='k', label='CW Fit')
-            ax.plot(bw_df_offsets, bw_df_lumis, marker='o', c='r', ls='none', label='Sim Rates')
-            ax.plot(xs_plot, vernier_scan_fit_bkg(xs_plot, *popt_sim), '--', c='r', label='Sim Fit')
-            ax.set_xlabel('Offset (um)')
-            ax.set_ylabel('Rate (Hz)')
+            ax.plot(offsets, rates, marker='o', c='k', ls='none', label='MBD Measured Rates')
+            ax.plot(xs_plot, vernier_scan_fit_bkg(xs_plot, *popt_cw), '--', c='k', label='MBD Fit')
+            ax.plot(bw_df_offsets, bw_df_lumis, marker='o', c='r', ls='none', label=r'Numerical Integration $\mathcal{L}_{\rm{naked}}$')
+            ax.plot(xs_plot, vernier_scan_fit_bkg(xs_plot, *popt_sim), '--', c='r', label='Numerical Integration Fit')
+            ax.set_xlabel('Set Position [μm]')
+            ax.set_ylabel('Rate [Hz]')
             ax.annotate(width_str, (0.02, 0.97), xycoords='axes fraction', ha='left', va='top', fontsize=10,
                         bbox=dict(facecolor='white', alpha=0.5, boxstyle='round'))
             ax.legend()
-            ax.set_ylim(bottom=0)
-            ax.set_title(f'{scan_date} {orientation} Offset vs Rate -- Default CAD Parameters (no fit)')
+            ax.set_ylim(bottom=0, top=np.max(rates) * 1.2)
+            ax.set_title(f'{scan_date} {orientation} Set Position vs Rate')
             fig.tight_layout()
+            fig.subplots_adjust(left=0.085, right=0.995, bottom=0.095, top=0.95)
+
+            if out_path:
+                fig.savefig(f'{out_path}{scan_date}_{orientation}_set_pos_vs_rate.png')
+                fig.savefig(f'{out_path}{scan_date}_{orientation}_set_pos_vs_rate.pdf')
+
         plt.show()
 
     print('donzo')

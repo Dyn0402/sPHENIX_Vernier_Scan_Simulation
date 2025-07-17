61c2d60d2c4ace5d5ccf777984ee977e5d76be0c
Dylan Neff <Dyn0402@hotmail.com>
2025-07-17 15:19:48 +0200

    Added calculate_default_cross_sections.py to calculate default cross sections
    Uncertainty analysis for later

---
+++ vernier_scan_analyses/auau24/calculate_default_cross_sections.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+"""
+Created on July 17 12:37 PM 2025
+Created in PyCharm
+Created as sPHENIX_Vernier_Scan_Simulation/calculate_default_cross_sections.py
+
+@author: Dylan Neff, dylan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from BunchCollider import BunchCollider
+from z_vertex_fitting_common import get_profile_path, set_sim
+from common_logistics import set_base_path
+from Measure import Measure
+
+
+def main():
+    base_path = set_base_path()
+    base_path += 'Vernier_Scans/auau_oct_16_24/'
+    calculate_default_cross_sections(base_path)
+    print('donzo')
+
+
+def calculate_default_cross_sections(base_path):
+    """ Calculate the default cross sections for MBD and ZDC detectors in the sPHENIX experiment.
+    Use head on steps to calculate the cross sections.
+    :param base_path: The base path to the simulation directory.
+    """
+    longitudinal_profiles_dir_path = f'{base_path}profiles/'
+    combined_cad_step_data_csv_path = f'{base_path}combined_cad_step_data.csv'
+    beam_width_csv_path = f'{base_path}beam_widths_rate_only_fit_results.csv'
+
+    f_beam = 78.4 * 1e3  # Hz  Frequency of single bunch
+    mb_to_um2 = 1e-19
+    n_bunches = 111  # Number of bunches in the collider
+    # lumi_z_cut = 200
+    lumi_z_cut = None
+    observed = False  # Should always be False for cross section calculations
+
+    cad_df = pd.read_csv(combined_cad_step_data_csv_path)
+    beam_width_df = pd.read_csv(beam_width_csv_path)
+
+    rate_name_for_beam_width = 'MBD |z|<200 Sasha Corrected'
+    rate_names_for_absolute_rate = ['mbd_sasha_bkg_cor_rate', 'zdc_sasha_cor_rate']
+
+    collider_sim = BunchCollider()
+    collider_sim.set_grid_size(31, 31, 101, 31)
+    beta_star = 76.4
+    bkg = 0.0e-17
+    gauss_eff_width = 500
+    mbd_resolution = 1.0
+
+    beam_width_df_rate = beam_width_df[beam_width_df['rate_name'] == rate_name_for_beam_width]
+    beam_width_x = beam_width_df_rate[beam_width_df['orientation'] == 'Horizontal'].iloc[0]['beam_width']
+    beam_width_y = beam_width_df_rate[beam_width_df['orientation'] == 'Vertical'].iloc[0]['beam_width']
+
+    scan_steps = cad_df['step']
+
+    collider_sim.set_bkg(bkg)
+    collider_sim.set_gaus_z_efficiency_width(gauss_eff_width)
+    collider_sim.set_gaus_smearing_sigma(mbd_resolution)
+    collider_sim.set_bunch_beta_stars(beta_star, beta_star)
+
+    # Get nominal dcct ions and emittances
+    step_0 = cad_df[cad_df['step'] == 0].iloc[0]
+    em_blue_horiz_nom, em_blue_vert_nom = step_0['blue_horiz_emittance'], step_0['blue_vert_emittance']
+    em_yel_horiz_nom, em_yel_vert_nom = step_0['yellow_horiz_emittance'], step_0['yellow_vert_emittance']
+    em_blue_nom = (em_blue_horiz_nom, em_blue_vert_nom)
+    em_yel_nom = (em_yel_horiz_nom, em_yel_vert_nom)
+
+    steps_plt, lumis, lumi_errs = [], [], []
+    rates = {rate_name: [] for rate_name in rate_names_for_absolute_rate}
+    rate_errs = {rate_name: [] for rate_name in rate_names_for_absolute_rate}
+    for step in scan_steps:
+        cad_step_row = cad_df[cad_df['step'] == step].iloc[0]
+        if cad_step_row['set offset h'] != 0 or cad_step_row['set offset v'] != 0:
+            continue  # Only calculate for head-on steps
+        profile_paths = get_profile_path(
+            longitudinal_profiles_dir_path, cad_step_row['start'], cad_step_row['end'], True
+        )
+        profile_lumis = []
+        for profile_path in profile_paths:
+            set_sim(collider_sim, cad_step_row, beam_width_x, beam_width_y, em_blue_nom, em_yel_nom, profile_path)
+            collider_sim.run_sim_parallel()
+            if lumi_z_cut is None:
+                naked_lumi = collider_sim.get_naked_luminosity()
+            else:
+                zs, z_dist = collider_sim.get_z_density_dist()
+                zs_cut, z_dist_cut = zs[np.abs(zs) < lumi_z_cut], z_dist[np.abs(zs) < lumi_z_cut]
+                naked_lumi = collider_sim.get_naked_luminosity(observed=observed)
+                cut_fraction = np.trapezoid(z_dist_cut, zs_cut) / np.trapezoid(z_dist, zs)
+                naked_lumi *= cut_fraction
+            n_blue, n_yellow = cad_step_row['blue_wcm_ions'], cad_step_row['yellow_wcm_ions']  # Total for all bunches
+            lumi = naked_lumi * mb_to_um2 * f_beam * n_blue * n_yellow / n_bunches * 1e3  # in b⁻¹s⁻¹
+            profile_lumis.append(lumi)
+        lumi_mean, lumi_std = np.nanmean(profile_lumis), np.nanstd(profile_lumis)
+        steps_plt.append(step)
+        lumis.append(lumi_mean)
+        lumi_errs.append(lumi_std)
+        for rate_col in rate_names_for_absolute_rate:
+            rate_val = cad_step_row[rate_col]
+            dur = cad_step_row['rate_calc_duration']
+            rate_err = np.sqrt(cad_step_row[rate_col] * dur) / dur
+            rates[rate_col].append(rate_val)
+            rate_errs[rate_col].append(rate_err)
+
+    # Plot rates and lumis vs step
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.errorbar(steps_plt, lumis, yerr=lumi_errs, fmt='o', label='Lumi')
+    ax.set_xlabel('Step')
+    for rate_col in rate_names_for_absolute_rate:
+        scale = lumis[0] / rates[rate_col][0]  # Scale rates to match lumi
+        ax.errorbar(steps_plt, np.array(rates[rate_col]) * scale, yerr=np.array(rate_errs[rate_col]) * scale, fmt='o',
+                    label=rate_col)
+    ax.set_ylabel('Lumi ($b^-1s^-1$) & Rate (Hz) (scaled)')
+    ax.set_title('Lumi and Rates vs Step')
+    ax.legend()
+    fig.tight_layout()
+
+    # Calculate cross sections
+    lumi_meases = np.array([Measure(val, err) for val, err in zip(lumis, lumi_errs)])
+    cross_sections = {}
+    for rate_col in rate_names_for_absolute_rate:
+        rate_meases = np.array([Measure(val, err) for val, err in zip(rates[rate_col], rate_errs[rate_col])])
+        cross_sections[rate_col] = rate_meases / lumi_meases  # Should be in units of mb
+
+    # Plot cross sections vs step
+    for rate_col in rate_names_for_absolute_rate:
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.errorbar(steps_plt, [cs.val for cs in cross_sections[rate_col]],
+                    yerr=[cs.err for cs in cross_sections[rate_col]], fmt='o')
+        ax.set_xlabel('Step')
+        ax.set_ylabel('Cross Section (mb)')
+        ax.set_title(f'Cross Section for {rate_col}')
+        fig.tight_layout()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
